01b55ca907c509fad4cca1701b6913c5f7edc277
Mark Lee <mark@9embers.com>
2024-11-19 22:23:36 -0500

    Many subtitles are sacrificed

--- Cthulhu/MainPage.xaml.cs
+++ Cthulhu/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using Windows.Media.Effects;
 
 namespace Cthulhu;
 
@@ -14,38 +16,7 @@ public partial class MainPage : ContentPage
         {
             this.RegisterDrop( Handler?.MauiContext, async ( path ) =>
             {
-                MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = "The beast eats..."; } );
-                await Task.Delay( 10 );
-
-                var ffmpeg = System.IO.Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "unmanaged/ffmpeg.exe" );
-                var output = System.IO.Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "Fixed" );
-
-
-                var format = path.Split( "." ).Last();
-                output += "." + format;
-
-                if ( File.Exists( output ) )
-                {
-                    File.Delete( output );
-                }
-
-                ProcessStartInfo ProcessInfo;
-                Process Process;
-
-                ProcessInfo = new ProcessStartInfo( ffmpeg, $" -i \"{path}\" -c copy -sn \"{output}\"" );
-                ProcessInfo.CreateNoWindow = true;
-                ProcessInfo.UseShellExecute = false;
-
-                Process = Process.Start( ProcessInfo );
-
-                Process.WaitForExit();
-
-                File.Delete( path );
-                File.Copy( output, path );
-                File.Delete( output );
-
-                await Task.Delay( 10 );
-                MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = "Cthulhu is hungry for subtitles"; } );
+                Task.Run(() => EnqueVideo( path ));
             } );
         };
 
@@ -56,8 +27,83 @@ public partial class MainPage : ContentPage
 #endif
     }
 
+    bool isRunning;
+    ConcurrentQueue<string> paths = new ConcurrentQueue<string>();
+
+    public async Task EnqueVideo( string path )
+    {
+        await Task.Delay( 10 );
+        paths.Enqueue( path );
+
+        if ( paths.Count > 1 )
+        {
+            MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = $"{paths.Count} are sacrificed"; } );
+        }
+        else
+        {
+            MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = "The beast eats..."; } );
+        }
+
+        await Task.Delay( 10 );
+
+        if ( !isRunning )
+        {
+            isRunning = true;
+            await ProcessVideos();
+        }
+    }
+
+    public async Task ProcessVideos()
+    {
+        while ( paths.TryDequeue( out string? path ) )
+        {
+            await ProcessVideo( path );
+
+            if ( paths.Count > 1 )
+            {
+                MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = $"{paths.Count} are sacrificed"; } );
+            }
+            else
+            {
+                MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = "The beast eats..."; } );
+            }
+            await Task.Delay( 10 );
+        }
+
+        isRunning = false;
+        MainThread.BeginInvokeOnMainThread( () => { lStatus.Text = "Cthulhu is hungry for subtitles"; } );
+        await Task.Delay( 10 );
+    }
+
+    public async Task ProcessVideo( string path )
+    {
+        var ffmpeg = System.IO.Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "unmanaged/ffmpeg.exe" );
+        var output = System.IO.Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "Fixed" );
 
 
+        var format = path.Split( "." ).Last();
+        output += "." + format;
+
+        if ( File.Exists( output ) )
+        {
+            File.Delete( output );
+        }
+
+        ProcessStartInfo ProcessInfo;
+        Process Process;
+
+        ProcessInfo = new ProcessStartInfo( ffmpeg, $" -i \"{path}\" -c copy -sn \"{output}\"" );
+        ProcessInfo.CreateNoWindow = true;
+        ProcessInfo.UseShellExecute = false;
+
+        Process = Process.Start( ProcessInfo );
+
+        Process.WaitForExit();
+
+        File.Delete( path );
+        File.Copy( output, path );
+        File.Delete( output );
+    }
 }
 
 
