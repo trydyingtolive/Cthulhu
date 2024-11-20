2d683c564ebb1056b9332d59e1719d072c372b93
Mark Lee <mark@9embers.com>
2024-11-19 19:59:32 -0500

    The ancient one is hungry

---
+++ Cthulhu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Cthulhu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Cthulhu/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cthulhu
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
